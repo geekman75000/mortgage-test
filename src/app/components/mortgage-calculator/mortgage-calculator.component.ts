c10e7d6f07d23fd1a28eff1624f3b914d47bbe53
Benoit Burgat <benoit.burgat@gmail.com>
2025-02-03 12:57:31 +0100

    created a Boostrap form

--- src/app/components/mortgage-calculator/mortgage-calculator.component.ts
+++ src/app/components/mortgage-calculator/mortgage-calculator.component.ts
@@ -1,20 +1,30 @@
-import { JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MortgageFormServiceService } from '../../services/forms/mortgage-form.service.service';
 
 @Component({
     selector: 'app-mortgage-calculator',
-    imports: [JsonPipe],
+    imports: [ReactiveFormsModule, CommonModule],
     templateUrl: './mortgage-calculator.component.html',
     styleUrl: './mortgage-calculator.component.scss'
 })
 export class MortgageCalculatorComponent {
 
     public mortgageForm: FormGroup;
+    public isFormSubmitted = false;
 
     constructor(private _mortgageFormService: MortgageFormServiceService) {
         this.mortgageForm = this._mortgageFormService.getForm();
     }
 
+    onSubmit() {
+        this.isFormSubmitted = true;
+
+        console.log(this.mortgageForm.value)
+    }
+
+    get f() {
+        return this.mortgageForm.controls;
+    }
 }
